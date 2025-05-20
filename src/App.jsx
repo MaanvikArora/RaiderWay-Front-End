a8daddb1b82656886220a878d6b2d4ad2c2d44b4
scar <scarletmacaw73@gmail.com>
2025-05-20 11:51:20 -0400

    initial branch commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,8 @@ function App() {
   const [activePage, setActivePage] = useState("home")
 
   return (
-    <>
+      <>
+        <div className="flex flex-col h-screen">
           <div className="navbar bg-base-100 shadow-sm bg-white">
             <div className="navbar-start">
               <div className="dropdown">
@@ -19,8 +20,8 @@ function App() {
                   <img src='src\assets\menuicon.JPG' className='h-5 w-5'></img>
                 </div>
                 <ul
-                  tabIndex={0}
-                  className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow bg-indigo-500">
+                    tabIndex={0}
+                    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow bg-indigo-500">
                   <li className='bg-red-700 hover:bg-white text-slate-950 rounded-sm'><a>Home</a></li>
                   <li className='bg-yellow-400 hover:bg-white text-slate-950 rounded-sm'><a>About</a></li>
                   <li className='text-white bg-slate-950 hover:text-slate-950 bg-white rounded-sm'><a>Contact</a></li>
@@ -34,15 +35,17 @@ function App() {
               <button onClick={() => onNav("interest")} className="btn btn-ghost bg-indigo-500">
                 Sign Up
               </button>
-              <div className='h-full bg-yellow-400'></div>
+
             </div>
           </div>
-          
-          {activePage === "home" && <Home onNav={() => setActivePage("choice")}/>}
-          {activePage === "choice" && <Choice onNav={setActivePage}></Choice>}
-          {activePage === "interest" && <Interests onNav={setActivePage}></Interests>}
-          {activePage === "program" && <Programs onNav={setActivePage}></Programs>}
-    </>
+          <div className="flex flex-col grow">
+            {activePage === "home" && <Home onNav={() => setActivePage("choice")}/>}
+            {activePage === "choice" && <Choice onNav={setActivePage}></Choice>}
+            {activePage === "interest" && <Interests onNav={setActivePage}></Interests>}
+            {activePage === "program" && <Programs onNav={setActivePage}></Programs>}
+          </div>
+        </div>
+      </>
   )
 }
 
