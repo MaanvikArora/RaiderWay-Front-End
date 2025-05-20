b359205df0c9d44ae8131e7dc440043058f54456
Scar-Macaw <scarletmacaw73@gmail.com>
2025-05-19 21:00:05 -0400

    Fixed errors in screensize and made site more dynamic in home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,18 +1,19 @@
 function Home({onNav}){
     return(
-        <div className="h-full">
-            <br />
-            <br />
-            <h1 className="text-5xl font-bold font-mono text-slate-950 text-center">  Your Journey Starts Here....</h1>
-            <div className="w-250 h-30 text-wrap">
-                <p className="py-6 font-mono text-slate-950 text-center ml-76">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                quasi. In deleniti eaque aut repudiandae et a id nisi.
-                </p>
+        <div className="flex flex-col items-center space-y-6 grow">
+
+            <h1 className="mt-5 text-5xl font-bold font-mono text-slate-950 text-center">  Your Journey Starts Here....</h1>
+            <p className="text-center py-6 font-mono text-slate-950 px-4">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+
+
+            <div className="flex grow my-2 justify-center">
+                <button onClick={onNav} className="btn btn-neutral">
+                    Generate RoadMap
+                </button>
             </div>
-            <button onClick={onNav} className="btn btn-neutral flex justify-self-center size 14 mb-58" >Generate RoadMap</button>
-            <div className="w-full bg-amber-500">
-                <p className="font-mono text-center"> NOTE: We do not represent KCI or the WRDSB. This is a tool built by the KCI CS Club. <br /> Please double check information before submitting courses.</p>
+
+            <div className="bg-amber-500 w-full">
+                <p className="font-mono text-center px-4"> NOTE: We do not represent KCI or the WRDSB. This is a tool built by the KCI CS Club. <br /> Please double check information before submitting courses.</p>
             </div>
         </div>
     );
